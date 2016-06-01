4e8ea1fec43df634800df5a7d51bf37d2358889b
堀家隆宏 <horike@horiketakahiro-no-MacBook-Pro.local>
2016-06-01 18:58:47 +0900

    some fix

--- tests/test-wp_elasticsearch.php
+++ tests/test-wp_elasticsearch.php
@@ -2,31 +2,6 @@
 
 class WP_Elasticsearch_Test extends WP_UnitTestCase {
 	
-	/**
-	 * Target post_id
-	 *
-	 */
-	private $search_post_id;
-
-	/**
-	 * Search test. 
-	 *
-	 * @since 0.1
-	 */
-	function test_search() {
-		/*remove_filter( 'wpels_search', array( WP_Elasticsearch::get_instance(), 'search' ) );
-		add_filter( 'wpels_search', array( $this, 'el_search_mock' ) );
-
-		$this->search_post_id = $this->factory->post->create();
-		$this->go_to( '/?s=searchText' );
-		$this->assertQueryTrue( 'is_search' );
-		while ( have_posts() ) {
-			the_post();
-			$this->assertEquals( $this->search_post_id,  get_the_ID() );
-		}*/
-	}
-
-	
 	private $client;
 
 	/**
@@ -67,7 +42,6 @@ class WP_Elasticsearch_Test extends WP_UnitTestCase {
 										'post_title' => 'es test',
 										'post_content' => 'everyday everywhere es test' )
 									);
-		$_POST['wpels_settings']["endpoint"] = ES_HOST;
 		$this->assertEquals( true, $this->client->_data_sync() );
 	}
 }
